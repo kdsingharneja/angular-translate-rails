b89a5e1bce34ff9b1241ee8ef9a7a8bc41dd92f2
KD Singh Arneja <kanwaldeep@gmail.com>
2015-02-27 11:42:15 -0500

    Removed comments
    Removed comments.
    
    For cleaner code.

--- test/integration/resource_test.rb
+++ test/integration/resource_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class NavigationTest < ActionDispatch::IntegrationTest
+class ResourceTest < ActionDispatch::IntegrationTest
   test 'can access angular-translate' do
     get '/assets/angular-translate.js'
     assert_response :success
@@ -8,7 +8,6 @@ class NavigationTest < ActionDispatch::IntegrationTest
 
   test 'angular-translate response is for the expected version' do
     get '/assets/angular-translate.js'
-    assert_match(/VERSION = '2\.9\.0'/, @response.body)
+    assert_match(/VERSION = '2\.6\.0'/, @response.body)
   end
 end
-
